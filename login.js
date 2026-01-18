164c827e620d73dd44dbd6acfc67087981790a15
Richcodz-cloud <chillysri91@gmail.com>
2026-01-18 17:28:42 +0530

    Create login.js
    Signed-off-by: Richcodz-cloud <chillysri91@gmail.com>

---
+++ login.js
@@ -0,0 +1,18 @@
+const ADMIN_PASSWORD = "admin123";
+const MEMBER_PASSWORD = "member123";
+
+function memberLogin(){
+    const pwd = document.getElementById("loginPwd").value;
+    if(pwd === MEMBER_PASSWORD || pwd === ADMIN_PASSWORD){
+        localStorage.setItem("loginType","member");
+        window.location.href="index.html";
+    } else alert("Wrong password");
+}
+
+function adminLogin(){
+    const pwd = document.getElementById("loginPwd").value;
+    if(pwd === ADMIN_PASSWORD){
+        localStorage.setItem("loginType","admin");
+        window.location.href="index.html";
+    } else alert("Wrong password");
+}
